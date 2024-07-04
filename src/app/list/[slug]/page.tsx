0db81b9f4e13cb8540c8cba32c3c2f7f4f159eb1
rizalazky <rizalazky26@gmail.com>
2024-07-04 22:20:29 +0700

    take out unnecesary console log

--- src/app/list/[slug]/page.tsx
+++ src/app/list/[slug]/page.tsx
@@ -6,8 +6,7 @@ async function ListPage({params}:{params:{ slug : string}}) {
   const itemList = await getListDetail(params.slug)
   const pageTitle = itemList?.data?.list_desc
   const items = itemList?.data?.Items
-  console.log('PAGE TITLE',itemList.data.list_desc)
-  console.log('ITEM LIST',itemList.data.Items)
+  
   return (
     <Main pageTitle={pageTitle} items={items}/>
   )
