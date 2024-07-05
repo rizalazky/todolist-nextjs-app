5371ee011da5b1e9506aaca619ef6271790e3486
rizalazky <rizalazky26@gmail.com>
2024-07-05 11:06:48 +0700

    main page : update padding

--- src/ui/main/index.tsx
+++ src/ui/main/index.tsx
@@ -13,13 +13,13 @@ async function Main({items,pageTitle}:{items:ItemInterface[],pageTitle :string})
     <>
       <Sidebar lists={getListAPI.data}/>
       <div className="flex-1 overflow-scroll">
-          <div className="sticky top-0 px-10 py-4 bg-gray-50 w-full">
+          <div className="sticky top-0 px-6 py-4 bg-gray-50 w-full">
             <h1 className='text-2xl font-bold'>{pageTitle}</h1>
           </div>
-          <main className="flex flex-col w-full h-full px-10">
+          <main className="flex flex-col w-full h-full px-6">
             <ContainerList items={items}/>
           </main>
-          <div className="sticky bottom-10 px-20 w-full">
+          <div className="sticky bottom-0 px-6 py-4 w-full">
             <Input/>
           </div>
       </div>
