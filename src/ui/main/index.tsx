3275da3352094a6fdc2c40e29db1111c6f29b594
rizalazky <rizalazky26@gmail.com>
2024-07-06 21:44:55 +0700

    update main

--- src/ui/main/index.tsx
+++ src/ui/main/index.tsx
@@ -8,18 +8,19 @@ interface ItemInterface {
   id:number
 }
 
-async function Main({items,pageTitle,id}:{items:ItemInterface[],pageTitle :string,id:number}) {
+async function Main({items,pageTitle,id}:{items:ItemInterface[],pageTitle :string,id:string}) {
   const getListAPI = await getList();
   return (
     <>
       <Sidebar lists={getListAPI.data}/>
       <div className="flex-1 overflow-scroll">
           <HeaderMain pageTitle={pageTitle} id={id}/>
+          
           <main className="flex flex-col w-full h-full px-6">
             <ContainerList items={items}/>
           </main>
           <div className="sticky bottom-0 px-6 py-4 w-full">
-            <Input/>
+            <Input idList={id}/>
           </div>
       </div>
     </>
