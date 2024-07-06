6d758c8ec2f3c50cedf819bfa628950c6c34385f
rizalazky <rizalazky26@gmail.com>
2024-07-06 21:43:28 +0700

    change props type data

--- src/components/headermain/index.tsx
+++ src/components/headermain/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import React from 'react'
 import { HiDotsHorizontal } from "react-icons/hi";
 
-function index({pageTitle, id}:{pageTitle:string,id:number}) {
+function index({pageTitle, id}:{pageTitle:string,id:string}) {
   return (
     <div className="sticky top-0 px-6 py-4 bg-gray-50 w-full flex justify-between items-center">
         <h1 className='text-2xl font-bold'>{pageTitle}</h1>
