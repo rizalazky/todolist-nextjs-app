0406327e2a136cbf30d07893ee22546c21177f52
rizalazky <rizalazky26@gmail.com>
2024-07-09 10:37:16 +0700

    modal delete list

--- src/components/headermain/index.tsx
+++ src/components/headermain/index.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { Dropdown } from 'flowbite-react'
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 import { HiDotsHorizontal } from "react-icons/hi";
+import { Modal } from '..';
 
 function index({pageTitle, id}:{pageTitle:string,id:string}) {
+  const [openModalDelete,setOpenModalDelete]=useState(false);
   return (
     <div className="sticky top-0 px-6 py-4 bg-gray-50 w-full flex justify-between items-center">
         <h1 className='text-2xl font-bold'>{pageTitle}</h1>
@@ -12,8 +14,9 @@ function index({pageTitle, id}:{pageTitle:string,id:string}) {
             <Dropdown.Item>
                 <Link href={`/updatelist/${id}`}>Update</Link>
             </Dropdown.Item>
-            <Dropdown.Item>Delete</Dropdown.Item>
+            <Dropdown.Item onClick={()=>setOpenModalDelete(true)}>Delete</Dropdown.Item>
         </Dropdown>
+        <Modal isModalDelete children='' headerTitle='' onCloseModal={()=>setOpenModalDelete(false)} openModal={openModalDelete}/>
     </div>
   )
 }
